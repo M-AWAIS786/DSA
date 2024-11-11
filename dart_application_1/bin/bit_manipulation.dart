213eaa3e9c3c831568fd9712a8cbef541975fcfb
M-AWAIS786 <awaisaslam0335@gmail.com>
2024-11-11 11:25:24 +0500

    bit manipulation

--- dart_application_1/bin/bit_manipulation.dart
+++ dart_application_1/bin/bit_manipulation.dart
@@ -1,23 +1,96 @@
 // ? Largest Combination With Bitwise AND Greater Than Zero
 import 'dart:math';
 
-import 'maths.dart';
+// class Solution {
+//   int largestCombination(List<int> candidates) {
+//     int ans = 1;
+//     for (int i = 0; i < 31; i++) {
+//       int setBit = 0;
+//       for (int c in candidates) {
+//         if (c & (1 << i) != 0) setBit++;
+//       }
+//       ans = max(ans, setBit);
+//     }
+//     return ans;
+//   }
+// }
+// class Solution {
+//   int search(List<int> nums, int target) {
+//     int start = 0;
+//     int end = nums.length - 1;
+//     while (start <= end) {
+//       int mid = ((start + end) / 2).floor();
+//       if (nums[mid] == target) {
+//         return mid;
+//       }
+//       if (nums[mid] < target) {
+//         start = mid + 1;
+//       } else {
+//         end = mid - 1;
+//       }
+//     }
+//     return -1;
+//   }
+// }
+
+// class Solution {
+//   int findContentChildren(List<int> g, List<int> s) {
+//     g.sort();
+//     s.sort();
+//     int contentChildren = 0; // who receives a cookie
+//     int cookieIndex = 0; // who gives a cookie
+
+//     while (cookieIndex < s.length && contentChildren < g.length) {
+//       if (s[cookieIndex] >= g[contentChildren]) {
+//         contentChildren++;
+//       }
+//       cookieIndex++;
+//     }
+//     return contentChildren;
+//   }
+// }
+
+// class Solution {
+//   int thirdMax(List<int> nums) {
+//     int? first;
+//     int? second;
+//     int? third;
+
+//     for (int num in nums) {
+//       if (num == first || num == second || num == third) continue;
+
+//       if (first == null || num > first) {
+//         third = second;
+//         second = first;
+//         first = num;
+//       } else if (second == null || num > second) {
+//         third = second;
+//         second = num;
+//       } else if (third == null || num > third) {
+//         third = num;
+//       }
+//     }
+//     return third ?? first!;
+//   }
+// }
 
 class Solution {
-  int largestCombination(List<int> candidates) {
-    int ans = 1;
-    for (int i = 0; i < 31; i++) {
-      int setBit = 0;
-      for (int c in candidates) {
-        if (c & (1 << i) != 0) setBit++;
+  int findMaxConsecutiveOnes(List<int> nums) {
+    int consCount = 0;
+    int prevCount = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
+        consCount++;
+        if (consCount > prevCount) prevCount = consCount;
+      } else if (nums[i] == 0) {
+        consCount = 0;
       }
-      ans = max(ans, setBit);
     }
-    return ans;
+    return prevCount;
   }
 }
 
 void main() {
   Solution s = Solution();
-  print(s.largestCombination([16, 17, 71, 62, 12, 24, 14]));
+  print(s.findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
 }
